69d252160e1dcc19474be187c1d1f9f6d178bea1
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-14 13:04:02 +0100

    refactored routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  #index
-  get 'tasks', to: 'tasks#index'
-  #create
-  post 'tasks', to: 'tasks#create'
-  #new
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  #edit
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  #show
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  #update
-  patch 'tasks/:id', to: 'tasks#update'
-  #destroy
-  delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 
   root to: 'tasks#index'
 end
