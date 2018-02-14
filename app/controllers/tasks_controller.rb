e3423949336b6fb3fe72e48914ffa7242a76e7aa
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-14 13:00:41 +0100

    created destroy method to delete a task from the database

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -26,6 +26,12 @@ class TasksController < ApplicationController
     @task.update(task_params)
 
     redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to root_path
   end
 
   private
